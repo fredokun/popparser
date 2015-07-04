d7f27ce925a94727f4438ec0505bce06595ebec6
fredokun <Frederic.Peschanski@lip6.fr>
2015-07-04 10:07:30 +0200

    first build successful

--- setup.py
+++ setup.py
@@ -66,7 +66,8 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    package_dir = {'':'src'},
+    packages=['popparser', 'popparser.expr'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
